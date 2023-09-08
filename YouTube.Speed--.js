9bb06615a97e493eb5ac14e383cdc297ea119d50
Tushar Sharma <tushar10.net@gmail.com>
2023-09-07 21:51:34 -0500

    Add files via upload

---
+++ YouTube.Speed--.js
@@ -0,0 +1,1 @@
+document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate = (document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate > 0.25) ? (document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate - 0.25):document.getElementsByClassName("video-stream html5-main-video")[0].playbackRate;
